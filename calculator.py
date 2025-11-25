ac0361c13bbfe61372bac039080a6edebe59f797
suhani <smathur_be22@thapar.edu>
2025-11-25 22:23:13 +0530

    Initial commit: add calculator and README

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Simple Calculator")
+    x = int(input("Enter first number: "))
+    y = int(input("Enter second number: "))
+
+    print("Addition:", add(x, y))
+    print("Subtraction:", subtract(x, y))
